44ed3b1bf6af106eb0c9f4bc8db8bb786f763306
xition999-creator <xition999@gmail.com>
2026-08-22 23:05:37 +0600

    Update Vtwo.js

--- Vtwo.js
+++ Vtwo.js
@@ -238,7 +238,7 @@
       <textarea id="input" placeholder="Ask something about the site here...(Alt + c to clear)" maxlength="500"></textarea>
     </div>
      <button id="button" aria-label="Open Chat">
-      <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <svg width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
       </svg>
     </button>
