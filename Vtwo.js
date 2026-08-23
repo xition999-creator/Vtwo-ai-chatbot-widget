84b9f97a6eee1d294cf24280bc83e01715dcf741
xition999-creator <xition999@gmail.com>
2026-08-23 10:32:29 +0600

    Update Vtwo.js

--- Vtwo.js
+++ Vtwo.js
@@ -49,7 +49,7 @@
     #div2 {
       display: flex;
       visibility: hidden;
-      border: 1px solid rgb(214, 214, 215);
+      border: 1px solid rgb(184, 190, 166);
       width: 380px;
       max-width: calc(100vw - 40px);
       height: 550px;
@@ -76,7 +76,7 @@
       min-height: 8vh;
       margin-top: auto;
       color: white;
-      background-color: rgb(26, 26, 28);
+      background-color: rgb(23, 23, 25);
       border: none;
       border-radius: 17px;
       width: 100%;
